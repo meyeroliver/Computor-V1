df7fa234ec43ccf543c7c9c29624faf09800aa57
omeyer <meyeroliver9401@gmail.com>
2018-09-16 17:50:15 +0200

    created a power method

--- Operations.py
+++ Operations.py
@@ -14,6 +14,18 @@ def ft_sqrt(num):
     return root
 
 
+def ft_power(num, power):
+    i = 0
+    summer = 1
+    while True:
+        if i >= power:
+            break
+        summer *= num
+        i += 1
+    return summer
+
+
+
 def main():
     return 0
 

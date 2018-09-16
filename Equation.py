f637c5f32175bcb9052deae4e9857f5f1c1ce8bf
omeyer <meyeroliver9401@gmail.com>
2018-09-16 11:31:44 +0200

    Created a print methods for the discriminant and the degree Created methods for checking the degree and the discriminant

--- Equation.py
+++ Equation.py
@@ -2,10 +2,20 @@ from Term import *
 
 
 class Equation:
+    """
+        Reduced eqaution format
+        The degree expresses that of the variable e.g (X, Y)
+        a -> degree 2
+        b -> degree 1
+        c -> degree
+    """
     def __init__(self, string):
         self.string = string
         self.equation_items = []
         self.reduced_equation = []
+        self.a = Term("0*X^2")
+        self.b = Term("0*X^1")
+        self.c = Term("0*X^0")
 
     def processing_string(self):
         term = ''
@@ -16,7 +26,7 @@ class Equation:
         for char in self.string:
             if found_equal == 1 and sign == '+':
                 sign = '-'
-            if found_equal == 1 and sign == '-':
+            elif found_equal == 1 and sign == '-':
                 sign = '+'
             if char == '=':
                 found_equal = 1
@@ -32,22 +42,63 @@ class Equation:
         final_term = sign + term
         self.equation_items.append(final_term.replace(' ', ''))
 
+
     def reduce_equation(self):
+
         terms = []
         """
-        The following creates term objects and stores them in a list
+        The following creates a term objects and stores them in a list of terms
         """
         for item in self.equation_items:
             term = Term(item)
             terms.append(term)
+        """
+        The following will group the like together
+        """
+        for item in terms:
+            if item.variable_degree == 2:
+                self.a.add_coefficient(item)
+            elif item.variable_degree == 1:
+                self.b.add_coefficient(item)
+            else:
+                self.c.add_coefficient(item)
+
+    def check_highest_degree(self):
+        if self.a.coefficient != 0:
+            return str(self.a.variable_degree)
+        elif self.b.coefficient != 0:
+            return str(self.b.variable_degree)
+        else:
+            return str(self.c.variable_degree)
+
+    def check_discriminant(self):
+        return self.b.coefficient * self.b.coefficient - 4 * self.a.coefficient * self.c.coefficient
+
+    def print_discriminant_result(self, discriminant):
+        if discriminant > 0:
+            print("A positive discriminant indicates that the quadratic has two distinct real number solutions.")
+        elif discriminant == 0:
+            print("A discriminant of zero indicates that the quadratic has a repeated real number solution.")
+        else:
+            print("A negative discriminant indicates that neither of the solutions are real numbers.")
+
+    def print_highest_degree(self, degree):
+        print("Polynomial degree -> " + str(degree))
 
     def print_equation_items(self):
         for item in self.equation_items:
             print(item)
 
+    def print_reduced_equation(self):
+        reduced_equation_string = self.a.to_string()[1:] + " " + self.b.to_string() + " " + self.c.to_string() + " = 0"
+        print("Reduced form ->  "+reduced_equation_string)
 
-sum = "1*X^2 - 3*X^1 + 2*X^0 = -2*X^1"
+
+sum = "1*X^2 - 5*X^1 + 2*X^0 = -2*X^1"
 myEquation = Equation(sum)
 myEquation.processing_string()
 myEquation.reduce_equation()
+myEquation.print_reduced_equation()
+myEquation.print_highest_degree(myEquation.check_highest_degree())
+myEquation.print_discriminant_result(myEquation.check_discriminant())
 # myEquation.print_equation_items()

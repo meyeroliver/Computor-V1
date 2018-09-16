83e0b6e910e43fea894ba548a8213a1a57bf22f2
omeyer <meyeroliver9401@gmail.com>
2018-09-16 11:29:45 +0200

    created a toString method to create printable term

--- Term.py
+++ Term.py
@@ -3,15 +3,19 @@ class Term:
         self.coefficient = int(string.split('*')[0])
         self.variable_degree = int(string.split('*')[1].split('^')[1])
         self.checksum = 0
+        self.term_str = ""
 
-    def printAttributes(self):
+    def to_string(self):
+        sign = ""
+        if self.coefficient >= 0:
+            sign = "+"
+        self.term_str = sign + str(self.coefficient) + " * X^" + str(self.variable_degree)
+        return self.term_str
+
+    def add_coefficient(self, term2):
+        self.coefficient += term2.coefficient
+
+    def print_attributes(self):
         print("The coefficient is -> ",  self.coefficient)
         print("The degree of x is -> ", self.variable_degree)
-
-    def addCoefficient(self, term2):
-        if self.coefficient != term2.coefficient:
-            return
-        return self.coefficient + term2.coefficient
-
-
 
